c776b10a2dfa8058cf0bb4fae545d0371357a616
abechanta <abe.chanta@hotmail.com>
2019-11-08 22:41:35 +0900

    apply sample stylesheet from extension path.
    open with:
    - examples/lorem ipsum-body only.html

--- src/extension.ts
+++ src/extension.ts
@@ -88,6 +88,11 @@ class ViewerPanel {
 		const pagedjsUri = vscode.Uri.file(
 			path.join(this._extensionPath, 'media', 'paged.polyfill.js')
 		).with({ scheme: 'vscode-resource' });
+		// TODO
+		// Should be read from workspace file.
+		const styleUri = vscode.Uri.file(
+			path.join(this._extensionPath, 'media', 'a4.css')
+		).with({ scheme: 'vscode-resource' });
 
 		// Use a nonce to whitelist which scripts can be run
 		const nonce = getNonce();
@@ -101,14 +106,17 @@ class ViewerPanel {
 		// ---> paged.polyfill.js:24272 Refused to apply inline style because it violates the following Content Security Policy directive: "default-src 'none'". Either the 'unsafe-inline' keyword, a hash ('sha256-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'), or a nonce ('nonce-...') is required to enable inline execution. Note also that 'style-src' was not explicitly set, so 'default-src' is used as a fallback.
 		// To surpress error message below, we'll grant 'data:' for img-src.
 		// ---> Refused to load the image '<URL>' because it violates the following Content Security Policy directive: "img-src vscode-resource:".
+		// To surpress error message below, we'll grant 'vscode-resource:' for connect-src & style-src.
+		// ---> Refused to connect to 'vscode-resource:................/media/a4.css' because it violates the following Content Security Policy directive: "default-src 'none'". Note that 'connect-src' was not explicitly set, so 'default-src' is used as a fallback.
 		this._panel.webview.html = `<!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src vscode-resource: data:; script-src 'nonce-${nonce}' 'unsafe-eval'; style-src 'unsafe-inline';">
+		<meta http-equiv="Content-Security-Policy" content="default-src 'none'; connect-src vscode-resource:; img-src vscode-resource: data:; script-src 'nonce-${nonce}' 'unsafe-eval'; style-src 'unsafe-inline' vscode-resource:;">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<script nonce="${nonce}" src="${pagedjsUri}"></script>
 		<script nonce="${nonce}" src="${scriptUri}"></script>
+		<link rel="stylesheet" href="${styleUri}" />
 	</head>
 	<body>
 ${bodyContent}
