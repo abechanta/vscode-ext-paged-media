2aa06ee4d29dd8ee704785d376bc94aee7958c4d
abechanta <abe.chanta@hotmail.com>
2019-11-10 19:49:27 +0900

    FIXME: prepend script line importing paged.js.

--- extension.js
+++ extension.js
@@ -1,6 +1,14 @@
 'use strict'
+const path = require("path");
+const vscode = require("vscode");
 
 function activate(context) {
+    // TODO
+    // File "media/paged.polyfill.js" should be copied automatically from node_modules folder.
+    const scriptUri = vscode.Uri.file(
+        path.join(context.extensionPath, "media", "paged.polyfill.js")
+    ).with({ scheme: 'vscode-resource' });
+
     return {
         extendMarkdownIt(md) {
             // md.use(require('markdown-it-'));
@@ -8,7 +16,16 @@ function activate(context) {
             const render = md.renderer.render;
             md.renderer.render = (tokens, options, env) => {
                 try {
-                    return render.call(md.renderer, tokens, options, env);
+                    const html = render.call(md.renderer, tokens, options, env);
+                    // FIXME: Injection here causes following console error,
+                    // "Refused to load the script 'vscode-resource://file/xxxxxxxxxxxxxxxx/media/paged.polyfill.js' because it violates the following Content Security Policy directive: "script-src 'nonce-xxxxxxxxxxxxxxxx'"."
+                    // then displays notification.
+                    // "Some content has been disabled in this document."
+                    // Users must set "Select security setting for Markdown previews in this workspace" from "Strict" to "Disable",
+                    // but another console error will occur.
+                    // "Failed to load resource: net::ERR_ACCESS_DENIED"
+                    const scriptLink = `<script src="${scriptUri}"></script>`;
+                    return [ scriptLink, html, ].join("\n");
                 } catch (err) {
                     throw err;
                 }
