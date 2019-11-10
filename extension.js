7fd7113a64a1172391ecc54ec1ee7be2710b28dd
abechanta <abe.chanta@hotmail.com>
2019-11-10 19:17:53 +0900

    add simple wrapper of markdown renderer.

--- extension.js
+++ extension.js
@@ -5,6 +5,15 @@ function activate(context) {
         extendMarkdownIt(md) {
             // md.use(require('markdown-it-'));
 
+            const render = md.renderer.render;
+            md.renderer.render = (tokens, options, env) => {
+                try {
+                    return render.call(md.renderer, tokens, options, env);
+                } catch (err) {
+                    throw err;
+                }
+            };
+
             const highlight = md.options.highlight;
             md.options.highlight = (code, lang) => {
                 // highlight reserves "pre" block to render... this is not good method for paged media...
