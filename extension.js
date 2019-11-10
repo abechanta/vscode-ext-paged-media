aaea85dec68f43af192e44e56a922af73271e5ea
abechanta <abe.chanta@hotmail.com>
2019-11-10 19:04:39 +0900

    add fence for inline style via highlight, temp.
    open with:
    - examples/lorem ipsum-inline style.md

--- extension.js
+++ extension.js
@@ -4,6 +4,16 @@ function activate(context) {
     return {
         extendMarkdownIt(md) {
             // md.use(require('markdown-it-'));
+
+            const highlight = md.options.highlight;
+            md.options.highlight = (code, lang) => {
+                // highlight reserves "pre" block to render... this is not good method for paged media...
+                if (lang && lang.match(/\bstyle\b/i)) {
+                    return `<style>${code}</style>`;
+                }
+                return highlight(code, lang);
+            };
+
             return md;
         }
     };
