076dbc7a2cc2a4ff7101e10eb428d305894f799d
abechanta <abe.chanta@hotmail.com>
2019-11-11 12:55:52 +0900

    convert code indentation to tab=4

--- extension.js
+++ extension.js
@@ -1,30 +1,30 @@
 'use strict'
 
 function activate(context) {
-    return {
-        extendMarkdownIt(md) {
-            // md.use(require('markdown-it-'));
+	return {
+		extendMarkdownIt(md) {
+			// md.use(require('markdown-it-'));
 
-            const render = md.renderer.render;
-            md.renderer.render = (tokens, options, env) => {
-                try {
-                    return render.call(md.renderer, tokens, options, env);
-                } catch (err) {
-                    throw err;
-                }
-            };
+			const render = md.renderer.render;
+			md.renderer.render = (tokens, options, env) => {
+				try {
+					return render.call(md.renderer, tokens, options, env);
+				} catch (err) {
+					throw err;
+				}
+			};
 
-            const highlight = md.options.highlight;
-            md.options.highlight = (code, lang) => {
-                // highlight reserves "pre" block to render... this is not good method for paged media...
-                if (lang && lang.match(/\bstyle\b/i)) {
-                    return `<style>${code}</style>`;
-                }
-                return highlight(code, lang);
-            };
+			const highlight = md.options.highlight;
+			md.options.highlight = (code, lang) => {
+				// highlight reserves "pre" block to render... this is not good method for paged media...
+				if (lang && lang.match(/\bstyle\b/i)) {
+					return `<style>${code}</style>`;
+				}
+				return highlight(code, lang);
+			};
 
-            return md;
-        }
-    };
+			return md;
+		}
+	};
 }
 exports.activate = activate;
--- loading.js
+++ loading.js
@@ -1,6 +1,6 @@
 window.Paged = require('pagedjs');
 window.addEventListener("load", () => {
-    console.log("hello from loading.js.");
-    const pv = new window.Paged.Previewer();
-    pv.preview();
+	console.log("hello from loading.js.");
+	const pv = new window.Paged.Previewer();
+	pv.preview();
 });
