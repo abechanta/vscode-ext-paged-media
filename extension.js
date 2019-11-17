01a2189bb62a97fe09ddb44ebe969454b803d499
abechanta <abe.chanta@hotmail.com>
2019-11-17 23:33:10 +0900

    apply paged.js previewer to markdown-it renderer.

--- extension.js
+++ extension.js
@@ -1,14 +1,55 @@
-'use strict'
+'use strict';
+const pagedjs = require("pagedjs");
+const JSDOM = require("jsdom").JSDOM;
+
+function prepareDomGlobals() {
+	const jsdom = new JSDOM("", { pretendToBeVisual: true, });
+
+	global.document = jsdom.window.document;
+	global.document.fonts = []
+	global.NodeFilter = jsdom.window.NodeFilter;
+	global.performance = jsdom.window.performance;
+	global.window = jsdom.window;
+
+	global.requestAnimationFrame = jsdom.window.requestAnimationFrame;
+	global.cancelAnimationFrame = jsdom.window.cancelAnimationFrame;
+
+	// FIXME
+	// TypeError: document.createRange is not a function
+	// TypeError: range.createContextualFragment is not a function
+	global.document.createRange = function() {
+		return {
+			setEnd: function() {},
+			setStart: function() {},
+			getBoundingClientRect: function() {
+				return { right: 0, };
+			}
+		}
+	};
+}
+
+async function renderHtml(html) {
+	try {
+		const pv = new pagedjs.Previewer();
+		const rendered = await pv.preview(html);
+		return rendered;
+	} catch (err) {
+		throw err;
+	}
+}
 
 function activate(context) {
-    return {
+	prepareDomGlobals();
+
+	return {
         extendMarkdownIt(md) {
             // md.use(require('markdown-it-'));
 
             const render = md.renderer.render;
             md.renderer.render = (tokens, options, env) => {
                 try {
-                    return render.call(md.renderer, tokens, options, env);
+					const html = render.call(md.renderer, tokens, options, env);
+					return renderHtml(html);
                 } catch (err) {
                     throw err;
                 }
