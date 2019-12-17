c63722bb76d692c52aae3ac89413be7e059d6a7a
abechanta <abe.chanta@hotmail.com>
2019-12-17 21:48:29 +0900

    export dummy file to html.

--- ext-exporter.js
+++ ext-exporter.js
@@ -1,8 +1,21 @@
 "use strict";
+const path = require("path");
+const vscode = require("vscode");
 const child_process = require("child_process");
 
 module.exports = function Exporter(context) {
+	function _exportHtml(uri) {
+		const html = uri.with({path: uri.path.toString().replace(/\.md$/, ".html")});
+		var data = "---HTML CONTENT COMES HERE---\n";
+		data = (new TextEncoder).encode(data);
+		return vscode.workspace.fs.writeFile(html, data).then(() => {
+			console.log(`export html done: ${html.fsPath}`);
+			return html;
+		});
+	}
+
 	function _exportPdf(uri) {
+		const pdf = uri.with({path: uri.path.toString().replace(/\.html$/, ".pdf")});
 		const proc = child_process.spawn("node", ["--version", ], { encoding: "utf8", cwd: context.extensionPath, });
 		console.log(`invoke cli: args: ${proc.spawnargs}`);
 		return new Promise((resolve, reject) => {
@@ -10,8 +23,8 @@ module.exports = function Exporter(context) {
 				console.log(data.toString());
 			});
 			proc.on("close", () => {
-				console.log(`export pdf done: ${uri.fsPath}`);
-				resolve(uri);
+				console.log(`export pdf done: ${pdf.fsPath}`);
+				resolve(pdf);
 			});
 			proc.on("error", err => {
 				reject(err.message);
@@ -29,7 +42,9 @@ module.exports = function Exporter(context) {
 
 	return {
 		exportFiles(uri, body) {
-			return _exportPdf(uri);
+			return _exportHtml(uri).then(html => {
+				return _exportPdf(html);
+			});
 		}
 	};
 };
