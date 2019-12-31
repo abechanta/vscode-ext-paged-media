f14da928fe1e24e719de6c7b946f95e9f0b36e00
abechanta <abe.chanta@hotmail.com>
2019-12-31 18:04:53 +0900

    add conditional rendering.

--- loading.js
+++ loading.js
@@ -1,22 +1,32 @@
+"use strict";
 window.Paged = require('pagedjs');
-window.addEventListener("load", () => {
-	console.log("hello from loading.js.");
-	const attrsList = {
-		"LINK": [ "rel", "href", ],
-		"STYLE": [ "innerHTML", ],
-	};
+(function() {
+	function salvageStyles() {
+		const attrsList = {
+			"LINK": [ "rel", "href", ],
+			"STYLE": [ "innerHTML", ],
+		};
 
-	// salvage styles from body content
-	let styles = Array.from(document.querySelectorAll("body link[rel='stylesheet'], body style"));
-	styles.forEach(style => {
-		const node = document.createElement(style.tagName);
-		attrsList[style.tagName].forEach(attr => {
-			node[attr] = style[attr];
+		// salvage styles from body content
+		let styles = Array.from(document.querySelectorAll("body link[rel='stylesheet'], body style"));
+		styles.forEach(style => {
+			const node = document.createElement(style.tagName);
+			attrsList[style.tagName].forEach(attr => {
+				node[attr] = style[attr];
+			});
+			document.head.appendChild(node);
+			style.remove();
 		});
-		document.head.appendChild(node);
-		style.remove();
-	});
+	}
 
-	const pv = new window.Paged.Previewer();
-	pv.preview();
-});
+	const firstNode = document.body.firstChild && document.body.firstChild.nextSibling || undefined;
+	console.log("firstNode: ", firstNode);
+	if (firstNode && (firstNode.nodeName === "#comment") && firstNode.data.includes("@toppage")) {
+		window.addEventListener("load", () => {
+			console.log("onload event invoked.");
+			salvageStyles();
+			const pv = new window.Paged.Previewer();
+			pv.preview();
+		});
+	}
+}());
