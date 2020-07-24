ad50ddbfbc23e0ba946b5396e193def6ad55a536
abechanta <abe.chanta@hotmail.com>
2020-07-24 17:47:00 +0900

    add an information popup for unrecommended settings.

--- extension.js
+++ extension.js
@@ -11,10 +11,13 @@ const Exporter = require("./ext-exporter");
 
 function activate(context) {
 	const exporter = Exporter(context);
-	var document = {
+	const includeRe = /^!\[\s*rel=content\s*\]\(\s*([^\s)]+)\s*[^\s)]*\s*\)$/im;
+	let document = {
 		uri: undefined,
-		body: undefined,
+		bodyHtml: undefined,
+		bodyMd: undefined,
 	};
+	let didRecommend = false;
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand('pagedView.exportPdf', () => {
@@ -31,7 +34,7 @@ function activate(context) {
 				};
 
 				progress.report({ increment: 10, message: "", });
-				return exporter.exportFiles(document.uri, document.body, { registerCancelHandler: registerCancelHandler, reporter: progress, }).then(message => {
+				return exporter.exportFiles(document.uri, document.bodyHtml, { registerCancelHandler: registerCancelHandler, reporter: progress, }).then(message => {
 					vscode.window.showInformationMessage(`${title}: done.\n${message}`);
 					return undefined;
 				}).catch(message => {
@@ -41,6 +44,20 @@ function activate(context) {
 			});
 		})
 	);
+
+	const recommendUserSettings = function (tokens, doc) {
+		const hasTopPage = tokens => (tokens[0].type == "html_block") && (tokens[0].content.includes("@toppage"));
+		const hasInclude = bodyMd => bodyMd.match(includeRe);
+		if (didRecommend || !hasTopPage(tokens) || !hasInclude(doc.bodyMd)) {
+			return;
+		}
+		didRecommend = true;
+		const markdownConfig = vscode.workspace.getConfiguration("markdown", doc.uri);
+		const scrollEditorWithPreview = !!markdownConfig.get("preview.scrollEditorWithPreview");
+		if (scrollEditorWithPreview) {
+			vscode.window.showInformationMessage("Recommended: When using W3C Paged Media Viewer, set \"markdown.preview.scrollEditorWithPreview\" to \"Off\". This will surpress unexpected scrolling of editor while editing.");
+		}
+	};
 
 	const slugify = function (str) {
 		str = str || "__blank__";
@@ -56,14 +73,16 @@ function activate(context) {
 			md.use(require("markdown-it-multimd-table"), { multiline: true, rowspan: true, headerless: true, });
 			md.use(require("markdown-it-footnote-conventional"));
 			md.use(require("./markdown-it-toc"), { slugify: slugify, selection: [1, 2, 3], });
-			md.use(require("markdown-it-include"), { includeRe: /^!\[\s*rel=content\s*\]\(\s*([^\s)]+)\s*[^\s)]*\s*\)$/im, root: () => path.dirname(vscode.window.activeTextEditor.document.fileName), });
+			md.use(require("markdown-it-include"), { includeRe: includeRe, root: () => path.dirname(vscode.window.activeTextEditor.document.fileName), });
 
 			const render = md.renderer.render;
 			md.renderer.render = (tokens, options, env) => {
 				try {
-					document.body = render.call(md.renderer, tokens, options, env);
+					document.bodyHtml = render.call(md.renderer, tokens, options, env);
+					document.bodyMd = vscode.window.activeTextEditor.document.getText();
 					document.uri = vscode.window.activeTextEditor.document.uri.with();
-					return document.body;
+					recommendUserSettings(tokens, document);
+					return document.bodyHtml;
 				} catch (err) {
 					throw err;
 				}
