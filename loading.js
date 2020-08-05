3c350b0f74215c4960d7f2742fd1134f362615e4
abechanta <abe.chanta@hotmail.com>
2020-08-06 00:01:56 +0900

    move loading code into PagedConfig.

--- loading.js
+++ loading.js
@@ -1,33 +1,16 @@
 "use strict";
 window.Paged = require('pagedjs');
+require('./paged-config');
 (function() {
-	function salvageStyles() {
-		const attrsList = {
-			"LINK": [ "rel", "href", ],
-			"STYLE": [ "innerHTML", ],
-		};
-
-		// salvage styles from body content
-		let styles = Array.from(document.querySelectorAll("body link[rel='stylesheet'], body style"));
-		styles.forEach(style => {
-			const node = document.createElement(style.tagName);
-			attrsList[style.tagName].forEach(attr => {
-				node[attr] = style[attr];
-			});
-			document.head.appendChild(node);
-			style.remove();
-		});
-	}
-
 	window.addEventListener("load", () => {
 		const rootNode = document.querySelector(".vscode-body");
 		const firstNode = rootNode && rootNode.firstChild && rootNode.firstChild.nextSibling || undefined;
 		console.log("firstNode: ", firstNode);
 		if (firstNode && (firstNode.nodeName === "#comment") && firstNode.data.includes("@toppage")) {
 			console.log("onload event invoked.");
-			salvageStyles();
-			const pv = new window.Paged.Previewer();
-			pv.preview();
+			window.PagedConfig.before();
+			const pv = new window.Paged.Previewer(/*{ "hyphenGlyph": "\u2011", }*/);
+			pv.preview().then(window.PagedConfig.after);
 		}
 	});
 }());
